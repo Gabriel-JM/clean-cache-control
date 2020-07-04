459f2f1cb814e1e96a0c254e015c8ebbc37785f6
Gabriel-JM <gabriel.josemelo@hotmail.com>
2020-07-04 18:09:55 -0300

    Move sut creation to a Factory Helper Function

--- src/domain/data/usecases/local-save-purchases.spec.ts
+++ src/domain/data/usecases/local-save-purchases.spec.ts
@@ -18,17 +18,30 @@ class CacheStoreSpy implements CacheStore {
   }
 }
 
+type SutTypes = {
+  sut: LocalSavePurchases,
+  cacheStore: CacheStoreSpy
+}
+
+function makeSut(): SutTypes {
+  const cacheStore = new CacheStoreSpy()
+  const sut = new LocalSavePurchases(cacheStore)
+
+  return {
+    sut,
+    cacheStore
+  }
+}
+
 describe('LocalSavePurchases', () => {
   test('should not delete cache on sut.init', () => {
-    const cacheStore = new CacheStoreSpy()
-    new LocalSavePurchases(cacheStore)
+    const { cacheStore } = makeSut()
 
     expect(cacheStore.deleteCallsCount).toBe(0)
   })
 
   test('should delete old cache before save', async () => {
-    const cacheStore = new CacheStoreSpy()
-    const sut = new LocalSavePurchases(cacheStore)
+    const { sut, cacheStore } = makeSut()
     await sut.save()
     expect(cacheStore.deleteCallsCount).toBe(1)
   })
