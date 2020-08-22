81a50dcdacb35f205a668ae66cea400480f14ac5
Gabriel-JM <gabriel.josemelo@hotmail.com>
2020-08-22 12:47:26 -0300

    Ensure LocalLoadPurchases will not delete cache upon creation

---
+++ src/data/usecases/load-purchases/local-validate-purchases.spec.ts
@@ -0,0 +1,24 @@
+import { LocalLoadPurchases } from '@/data/usecases'
+import { CacheStoreSpy } from '@/data/tests'
+
+type SutTypes = {
+  sut: LocalLoadPurchases,
+  cacheStore: CacheStoreSpy
+}
+
+function makeSut(timestamp = new Date()): SutTypes {
+  const cacheStore = new CacheStoreSpy()
+  const sut = new LocalLoadPurchases(cacheStore, timestamp)
+
+  return {
+    sut,
+    cacheStore
+  }
+}
+
+describe('LocalLoadPurchases', () => {
+  test('should not insert or delete cache on sut.init', () => {
+    const { cacheStore } = makeSut()
+    expect(cacheStore.actions).toEqual([])
+  })
+})
